de747e29ff614ab628dacd66b3845c9e6c4e03cd
Vishnu S <153924683+vishnus1793@users.noreply.github.com>
2025-02-25 19:44:30 +0530

    Time: 20 ms (15.60%) | Memory: 43.7 MB (29.04%) - LeetSync

---
+++ 415-add-strings/add-strings.java
@@ -0,0 +1,9 @@
+import java.math.BigInteger;
+class Solution {
+    public String addStrings(String num1, String num2) {
+        BigInteger n1 = new BigInteger(num1);
+        BigInteger n2 = new BigInteger(num2);
+        BigInteger r = n1.add(n2);
+        return r.toString();
+    }
+}
